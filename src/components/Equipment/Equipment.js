355dff28ec75700244498489330711e7564b78f4
John Nielsen <archangel@lestatdcourtsmb.lan>
2020-01-27 17:15:24 -0700

    Refactor Equipment To Class Based Component

--- src/components/Equipment/Equipment.js
+++ src/components/Equipment/Equipment.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import EquipmentItem from './EquipmentItem/EquipmentItem'
 
-const equipment = (props) => props.equipment.map ((equipmentItem, index) => {
-        return <EquipmentItem
-            name={equipmentItem.name}
-            type={equipmentItem.type}
-            click={() => props.clicked(index)}
-            key={equipmentItem.id}
-        />
-    });
+class Equipment extends Component {
+    render() {
+        return (
+            this.props.equipment.map ((equipmentItem, index) => {
+                return <EquipmentItem
+                    name={equipmentItem.name}
+                    type={equipmentItem.type}
+                    click={() => this.props.clicked(index)}
+                    key={equipmentItem.id}
+                />
+            })
+        )
+    }
+}
 
-export default equipment;
+export default Equipment;
